7372f56f0ede075af8d6d3069d2ebaa569f3aab2
Yves Maurer <>
2022-07-19 17:08:45 +0200

    adapted to run with text labels instead of numbers

--- ner-stats.py
+++ ner-stats.py
@@ -5,7 +5,7 @@ import json
 def dowork(args):
     lines = 0
     lines_with_ner = 0
-    codes = [0,0,0,0,0]
+    codes = {}
     for f in args.file:
         with open(f, 'r') as fil:
             for line in fil:
@@ -13,17 +13,17 @@ def dowork(args):
                 lines = lines + 1
                 found = False
                 for i in l['ner_tags']:
+                    if not i in codes:
+                        codes[i] = 0
                     codes[i] = codes[i] + 1
-                    if i > 0:
+                    if i != 'O':
                         found = True
                 if found:
                     lines_with_ner = lines_with_ner + 1
     print(f"Number of lines: {lines}")
     print(f"Number of lines with at least one NE: {lines_with_ner}")
-    st = 0
     for i in codes:
-        print(f"Number of tokens with status {st}: {i}")
-        st = st + 1
+        print(f"Number of tokens with status {i}: {codes[i]}")
 
 parser = ArgumentParser(description='parse a jsonl tokens / ner_tags file and extract statistics based on NER tags')
 parser.add_argument('file', nargs='*', help='jsonl files')
