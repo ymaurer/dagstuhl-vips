237fde09017649e9e42d71e48ddc7e54e46f48a1
Yves Maurer <>
2022-07-22 10:51:37 +0200

    v1

--- tagged-to-wordpos.py
+++ tagged-to-wordpos.py
@@ -2,6 +2,7 @@
 from argparse import ArgumentParser
 import json
 import re
+import math
 
 textblocks = {}
 
@@ -17,6 +18,9 @@ def centroid(box):
 
 def rescale(coord, width, height):
     return {'x': int(coord['x'])/int(width), 'y': int(coord['y'])/int(height)}
+
+def project(coord):
+    return math.sqrt((coord['x']+coord['y'])/2)
 
 def read_sentences(f):
     with open(f, 'r') as fil:
@@ -39,18 +43,27 @@ def read_sentences(f):
                 #print(counter, offset, tb['tokens'][i])
                 counter += len(tb['tokens'][i]) + 1
                 i += 1
-            print(j['id'], counter)
+            #print(j['id'], counter)
             # output the sentence and its tokens
-            print('trying to output:')
-            print(j['text'])
-            slen = len(j['text'])
-            while slen > 0:
-                print(i, tb['tokens'][i])
-                pos = rescale(centroid(tb['token_coords'][i]), tb['page_width'], tb['page_height'])
-                out['coord_x'] = pos['x']
-                out['coord_y'] = pos['y']
-                print(json.dumps(out))
-                slen = slen - len(tb['tokens'][i]) - 1
+            #print('trying to output:')
+            #print(j['text'])
+            slen = 0
+            entity_index = 0
+            while slen < len(j['text']) and entity_index < len(j['pos']):
+                #print(i, tb['tokens'][i])
+                if slen >= j['pos'][entity_index][0]:
+                    if j['tags'][entity_index].startswith('B-'):
+                        # print(tb['tokens'][i], tb['token_coords'][i], entity_index, j['tags'][entity_index])
+                        pos = rescale(centroid(tb['token_coords'][i]), tb['page_width'], tb['page_height'])
+                        out['coord_x'] = pos['x']
+                        out['coord_y'] = pos['y']
+                        out['diag_pos_page'] = project(pos)
+                        out['diag_pos_issue'] = out['diag_pos_page'] / int(tb['numpages'])
+                        out['text'] = tb['tokens'][i]
+                        out['ne_type'] = j['tags'][entity_index]
+                        print(json.dumps(out))
+                        entity_index = entity_index + 1
+                slen = slen + len(tb['tokens'][i]) + 1
                 i = i + 1
 
 
